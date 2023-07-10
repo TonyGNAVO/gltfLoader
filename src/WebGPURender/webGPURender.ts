7f1d07ce187367f3843d8de46297bc986f073a9c
TONY GNAVO <gnavo.tony@hotmail.fr>
2023-07-10 22:17:47 +0200

    Creation of depthView for renderPipeline.

--- src/WebGPURender/webGPURender.ts
+++ src/WebGPURender/webGPURender.ts
@@ -6,20 +6,47 @@ import { WebGPUDescriptor } from './Utils/utils';
 
 class WebGPURenderer {
     private gPUDevice = PrimitivCoreUtils.get_gPUDevice();
+    private format = PrimitivCoreUtils.get_gPUTextureFormat();
     private commandEncoder = this.gPUDevice.createCommandEncoder();
     private passEncoder;
     private gPUCanvasContext: GPUCanvasContext;
+    private depthView: GPUTextureView;
     constructor(descriptor: WebGPUDescriptor) {
-
         const context = descriptor.canvas.getContext('webgpu');
         if (!context) throw new Error();
 
         this.gPUCanvasContext = context;
 
+        // 1er étape : construire la view / texture
+        //____ création de la texture
+        const texture: GPUTexture = this.gPUDevice.createTexture({
+            format: this.format,
+            label: 'depth texture',
+            size: {
+                width: 1024,
+                height: 1024,
+            },
+            usage: GPUTextureUsage.RENDER_ATTACHMENT,
+            //sampleCount : see the doc. It's about aliasing
+        });
+
+        //____ création de la view
+        this.depthView = texture.createView();
+
+        // 2ème étape :  construire une pipeline de texture
+
+        // 3ème étape : utiliser ces infos pour executer la pipeline dans la fonction render avec la liste de toutes les geometry réuni
+
+        /**
+         * Build passEncoder
+         */
+
         this.passEncoder = this.commandEncoder.beginRenderPass({
             colorAttachments: [
                 {
-                    view: this.gPUCanvasContext.getCurrentTexture().createView(),
+                    view: this.gPUCanvasContext
+                        .getCurrentTexture()
+                        .createView(),
                     clearValue: { r: 0, g: 0, b: 0, a: 1.0 },
                     loadOp: 'clear',
                     storeOp: 'store',
@@ -35,6 +62,8 @@ class WebGPURenderer {
             },
         });
     }
+
+    private buildPassEncoder() {}
 
     render(scene: Scene) {
         scene.children.forEach((mesh) => {
